42ba812b10ee38095b8b340e5698d17155859eeb
y4ns3 <ukim3817@gmail.com>
2024-12-30 00:24:23 +0500

    1

---
+++ Others/PalindromeNumber.cpp
@@ -0,0 +1,37 @@
+/*
+Given an integer x, return true if x is a 
+palindrome, and false otherwise.
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+*/
+
+class Solution {
+public:
+ bool isPalindrome(int x) {
+
+	if (x < 0) return false;
+	int temp = x;
+    int reversed = 0;
+	while (temp > 0) {
+		reversed = reversed * 10 + temp % 10;
+		temp /= 10;
+	}
+	
+	return reversed == x;
+
+}
+};
--- README.md
+++ README.md
@@ -9,4 +9,5 @@ I'm not good at it yet, but I'm actively learning and improving my problem-solvi
 - **Array**: Problems related to arrays.
 - **Strings**: Problems related to string manipulation.
 - **DynamicProgramming**: Problems requiring dynamic programming approaches.
+- **Data_Structures**: Problems related to different data structures (stacks, queues, trees, etc.).
 - **Others**: Other types of problems.
